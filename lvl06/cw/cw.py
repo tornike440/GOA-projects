1ce8ac9f2f127eb30ee1b63c36f5f966f168fdc7
tornike440 <tokab98887@gmail.com>
2024-10-21 23:05:39 +0400

    commit

---
+++ lvl06/cw/cw.py
@@ -0,0 +1,42 @@
+#int, str, float,
+
+# a= 15
+
+
+# print("price of this T is " + str(a) + " dollars")
+
+
+# b=25
+
+# print(type(b))
+
+
+#create 3 diff variables which will have
+#diff data type, then print every single one of them
+#and their type()
+
+
+a="tornike"
+b=8
+c=9.0
+
+print(str(a) + " is " + str(type(a)))
+print(str(b) + " is " + str(type(b)))
+print(str(c) + " is " + str(type(c)))
+
+
+#int()
+#float()
+#str()
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lvl06/cw/cw2.py
@@ -0,0 +1,38 @@
+#create 3 variable that will have data type will be str, int and float
+#transformt str to int
+#transform int to float
+#transflorm float to str
+#print all of them and their data types
+
+a="56"
+b=8
+c=4.987
+
+a2= int(a)
+
+b2= float(b)
+
+c2= str(c)
+
+
+print("1-------"+ str(a) +"(a) had data type of " +"---"+ str(type(a)) +"---"+ " but it has been converted to(a2) data type of --- " + str(type(a2)) +"------1")
+
+print("2-------"+ str(b) +"(b) had data type of " +"---"+ str(type(b))+"---" + " but it has been converted to(b2) data type of --- " + str(type(b2)) +"------2")
+
+print("3-------"+ str(c) +"(c) had data type of " +"---" +str(type(c)) +"---"+ " but it has been converted to(c2) data type of --- " + str(type(c2)) +"------3")
+
+
+
+
+# print(a2)
+
+# print(b2)
+
+# print(c2)
+
+
+# print(type(a2))
+
+# print(type(b2))
+
+# print(type(c2))
