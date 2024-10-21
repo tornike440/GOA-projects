6c15ecd08c5908b912d4296bc73dcedec15478fe
tornike440 <tokab98887@gmail.com>
2024-10-22 00:40:29 +0400

    commit

---
+++ lvl06/hw/hw02.py
@@ -0,0 +1,8 @@
+#2)დაწერე პროგრამა, რომელიც შეამოწმებს, არის თუ არა მოცემული ცვლადი float ტიპის
+
+a=989.89
+
+if type(a) == float:
+    print(type(a))
+else:
+    print("it is not float")
---
+++ lvl06/hw/hw04.py
@@ -0,0 +1,14 @@
+#4)დაწერეთ პროგრამა სადაც მომხმარებელს შემოატანინებთ ინფორმაციას
+#  თავის თავზე (name,surname,age,address,gmail) 
+# და შემდეგ დაუპრინტეთ ეს ინფორმაცია ერთ დიდ წინადადებად
+
+name = input("name: ")
+surname = input("surname:")
+age = input("age: ")
+address = input("address: ")
+gmail=  input("gmail: ")
+
+print("so, with the information you gave me, your name is " + name
+       +"; your surname is "+ surname +
+      "; you are "+age+"years old, you live in"
+        + address +"and you mail is"+ gmail)
