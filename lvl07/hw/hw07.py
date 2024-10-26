329600e79d9f2e9f8344dc8acd2ba9c85beb1cf2
tornike440 <tokab98887@gmail.com>
2024-10-26 17:51:00 +0400

    commit

---
+++ lvl07/hw/hw07.py
@@ -0,0 +1,8 @@
+#1) შექნენით 2 ცვლადი თქვენი
+# სახელი და გვარი მოახდინეთ კონკატენაცია
+
+name= "tornike"
+
+surname = "beridze"
+
+print("my full name is: " +name +" "+surname)
---
+++ lvl07/hw/hw09.py
@@ -0,0 +1,21 @@
+#3) გაიხსენეთ და დაწერეთ რამდენ ნაწილად
+#  იყოფა მონაცემები და მიუწერეთ მაგალითები
+
+# integer(int),
+# float(float),
+# string(str),
+# boolean(bool)
+
+
+#int
+a=89
+
+#float
+b=8984.987
+
+#string
+c="string"
+
+#boolean
+d=a>b
+
