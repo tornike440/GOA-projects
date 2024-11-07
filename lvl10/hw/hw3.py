049eaa25bd6e8d1204b6df08111176fdf44a642a
tornike440 <tokab98887@gmail.com>
2024-11-07 21:49:24 +0400

    commit

--- lvl10/hw/hw3.py
+++ lvl10/hw/hw3.py
@@ -5,4 +5,4 @@ a = int(input(" enter first number"))
 
 b= int(input("enter second number"))
 
-print ( str(a)+ " first number that you inputted minus -" +str(b)+"- second number that you inputed is equal to" + str(a-b))
+print ( str(a)+ " first number that you inputted minus " +str(b)+" second number that you inputed is equal to" + str(a-b))
---
+++ lvl10/hw/hw5.py
@@ -0,0 +1,10 @@
+#მეგობრის სახელის შემოტანა
+#  დაწერე პროგრამა, რომელიც შემოატანინებს მომხმარებელს მისი საუკეთესო 
+#   მეგობრის სახელს და გვარს და დაპრინტავს ასეთი ფორმატით: "თქვენი მეგობრის სახელია X ხოლო გვარია Y".
+
+a = input("ur best friend name ")
+
+b = input("ur best friend surname ")
+
+
+print("ur best friend goes by the name of "+ a+", and his surname is "+b)
