a0e1f8404c04e17cb7b2b070e755f4a1500db565
tornike440 <tokab98887@gmail.com>
2024-11-15 15:31:49 +0400

    commit

---
+++ lvl13/hw/hw1.py
@@ -0,0 +1,9 @@
+#1 ) მომხმარებელს შემოატაინეთ ორი რიცხვი და მათზე მოახდინეთ მათემატიკური ოპერაციები.
+a = int(input("1st nubmer"))
+b = int(input("2nd number"))
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
---
+++ lvl13/hw/hw2.py
@@ -0,0 +1,15 @@
+#2)მოიყვანეთ 10 მაგალითი შედარების ოპერატორებზე ==,!=,>,<,=<,=>
+import random
+a= random.randint(0,100)
+
+b= random.randint(0,100)
+
+
+print(a>b)
+print(a>=b)
+
+print(a<b)
+print(a<=b)
+
+print(a==b)
+print(a!=b)
