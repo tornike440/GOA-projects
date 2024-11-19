212697451ae5125f9cd79b2d2744159e2750997d
tornike440 <tokab98887@gmail.com>
2024-11-19 22:18:39 +0400

    commit

---
+++ lvl14/cw/cw.py
@@ -0,0 +1,20 @@
+#1) გამოიყენეთ ორივე ლოგიკური ოპერატორი and და or და გააკეთეთ ყველა შესაძლო ვარიანტი ლოგიკური წინადადებების,
+#  and და or ის გამოყენებით გააკეთეთ სხვადასხვა შედარებები რომ მიიღოთ ბულეან ტიპის მონაცაემები true ან false
+
+a= int(input("enter number"))
+
+b= int(input("enter number"))
+
+c= int(input("enter number"))
+
+d= int(input("enter number"))
+
+
+
+print(a>b and c<d)
+
+print(a>b or c<d)
+
+print(a>=b and c<=d)
+
+print(a>=b or c<=d)
