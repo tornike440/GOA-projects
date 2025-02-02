626ed1b0381eeb56f11ba0fa55dec736a6937094
tornike440 <tokab98887@gmail.com>
2025-02-02 13:24:02 +0400

    commit

---
+++ lvl35/cw/cw1.py
@@ -0,0 +1,28 @@
+def add(a,b):
+    result=a+b
+    print(result)
+
+def subtract(a,b):
+    result=a-b
+    print(result)
+
+def multiply(a,b):
+    result=a*b
+    print(result)
+
+def devide(a,b):
+    result=a/b
+    print(result)
+
+num1=int(input("num:"))
+
+num2=int(input("num:"))
+
+add(num1,num2)
+
+subtract(num1,num2)
+
+multiply(num1,num2)
+
+devide(num1,num2)
+
