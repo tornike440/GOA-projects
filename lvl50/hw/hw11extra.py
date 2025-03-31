c5d3318153c8872a84cd41eef3dc25087f8cef1b
tornike440 <tokab98887@gmail.com>
2025-03-31 18:40:51 +0400

    commit

---
+++ lvl50/hw/hw11extra.py
@@ -0,0 +1,30 @@
+#Hey, young man, don't look around, I'm talking to you!
+
+#I am a programmer, work in this madhouse. my job is research code, and... at the same time I am also the other researcher's study object. There are a lot of the same people and work with me.
+
+# def find_super_man(s):
+#     final_string=""
+#     target_string="superman"
+#     index=0
+#     for i in range(len(s)):
+#         while index<(len(target_string)-1) and  s[i].lower() == target_string[index] and s[i+1].lower() == target_string[index+1]:
+#             final_string+=s[i]
+#             index+=1
+#             i+=1
+#         if index==len(target_string)-1:
+#             final_string+=s[i]
+#             break
+        
+#     print(final_string)
+# #test cases
+
+
+
+
+
+#/////////////////////////////////////
+list=["tornike","gio","luka","tornike"]
+print(list.count("tornike"))
+
+
+
---
+++ lvl51/cw/cw.py
@@ -0,0 +1,3 @@
+#worked on previous codewarses
+#explained lvl 42,lvl48 and lvl 49 codewars
+#understood everything that was said
