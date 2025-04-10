247b83cb89c83e8d4f953f706c9846cad653a32e
tornike440 <tokab98887@gmail.com>
2025-04-10 19:43:06 +0400

    commit

---
+++ lvl54/hw/hw4.py
@@ -0,0 +1,12 @@
+def multi(l_st):
+    # Assuming l_st is a number, return its square
+    return l_st * l_st
+
+def add(l_st):
+    # Assuming l_st is a number, return its double
+    return l_st + l_st
+
+def reverse(st):
+    # Assuming st is a list, return the reversed list
+    st.reverse()
+    return st
